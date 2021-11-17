cddbbf29c4a370d175f56e2c5aff56643b96b983
Po YZ <83596481+eazypau@users.noreply.github.com>
2021-11-17 21:32:22 +0800

    create project folder

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script>
+  import "./assets/css/tailwind.scss";
+  import Dialog from "./components/Dialog.vue";
+  import Disclosure from "./components/Disclosure.vue";
+  import Listbox from "./components/Listbox.vue";
+</script>
+<template>
+  <div class="p-16">
+    <!-- <NuxtWelcome /> -->
+    <div class="bg-white text-sky-600">
+      <p>testing</p>
+      <p class="text-2xl">animation</p>
+    </div>
+    <!-- Dialog box of headlessui maybe has some issue, need to avoid using it -->
+    <!-- <Dialog /> -->
+    <Disclosure />
+    <Listbox />
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: {
+    files: [
+      "./components/**/*.{vue,js}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./app.vue",
+      "./plugins/**/*.{js,ts}",
+      "./nuxt.config.{js,ts}",
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
