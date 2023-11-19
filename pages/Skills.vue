f0064ef5048c2afdcc14e80266d6e4bc7af210c3
Po YZ <poyizhi@gmail.com>
2023-11-19 23:10:00 +0800

    new: create projects and skills page

---
+++ pages/Skills.vue
@@ -0,0 +1,9 @@
+<script setup>
+//const
+const { skillWithLogo } = useConstants();
+</script>
+<template>
+  <NuxtLayout name="default">
+    <Skills :skill-set="skillWithLogo" />
+  </NuxtLayout>
+</template>
