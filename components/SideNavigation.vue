a3c9cce2c04ccdb9d1686528288083aa9ea30217
Po YZ <poyizhi@gmail.com>
2023-12-21 13:38:20 +0800

    new: add side arrow navigation for mobile view

---
+++ components/SideNavigation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="max-w-full overflow-x-hidden">
+    <NuxtLink
+      v-if="showLeftLink"
+      :href="leftLink"
+      class="opacity-50 fixed left-0 top-1/2 z-10 lg:hidden"
+    >
+      <ChevronLeftIcon class="w-10 h-10 text-white" />
+    </NuxtLink>
+    <NuxtLink
+      v-if="showRightLink"
+      :href="rightLink"
+      class="opacity-50 fixed right-0 top-1/2 z-10 lg:hidden"
+    >
+      <ChevronRightIcon class="w-10 h-10 text-white" />
+    </NuxtLink>
+    <slot />
+  </div>
+</template>
+<script setup>
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/24/outline";
+defineProps({
+  leftLink: {
+    type: String,
+    default: "",
+  },
+  showLeftLink: {
+    type: Boolean,
+    default: true,
+  },
+  rightLink: {
+    type: String,
+    default: "",
+  },
+  showRightLink: {
+    type: Boolean,
+    default: true,
+  },
+});
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,42 @@
 <template>
   <div>
     <NavBar />
-    <slot />
+    <SideNavigation
+      :left-link="leftLink"
+      :right-link="rightLink"
+      :show-right-link="showRightLink"
+    >
+      <slot />
+    </SideNavigation>
     <Footer />
   </div>
 </template>
+<script setup>
+const route = useRoute();
+const { navigation } = useConstants();
+const leftLink = ref("");
+const rightLink = ref("");
+const showRightLink = ref(true);
+
+const assignRoute = () => {
+  const findCurrentRouteIndex = navigation.findIndex(
+    (item) => item.href === route.path,
+  );
+  showRightLink.value = true;
+  if (findCurrentRouteIndex === 0) {
+    leftLink.value = "/";
+    rightLink.value = navigation[1].href;
+  } else if (findCurrentRouteIndex === navigation.length - 1) {
+    leftLink.value = navigation[findCurrentRouteIndex - 1].href;
+    showRightLink.value = false;
+    rightLink.value = "";
+  } else {
+    leftLink.value = navigation[findCurrentRouteIndex - 1].href;
+    rightLink.value = navigation[findCurrentRouteIndex + 1].href;
+  }
+};
+
+onMounted(() => {
+  assignRoute();
+});
+</script>
