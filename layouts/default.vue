382055b85d8dc704aa502c61a9d341042c8d3230
Po YZ <poyizhi@gmail.com>
2024-01-16 21:44:58 +0800

    new: render tag in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,30 @@
     <Footer />
   </div>
 </template>
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+const gtag = `
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "${runtimeConfig.public.NUXT_GTM_ID}");
+`;
+useHead({
+  script: [
+    {
+      async: true,
+      src: `https://www.googletagmanager.com/gtag/js?id=${runtimeConfig.public.NUXT_GTM_ID}`,
+    },
+    {
+      type: "text/javascript",
+      innerHTML: gtag,
+    },
+  ],
+});
+</script>
 <!-- <script setup>
 const route = useRoute();
 const { navigation } = useConstants();
--- layouts/main.vue
+++ layouts/main.vue
@@ -7,3 +7,28 @@
     <slot />
   </div>
 </template>
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+console.log(runtimeConfig.public);
+const gtag = `
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "${runtimeConfig.public.NUXT_GTM_ID}");
+`;
+useHead({
+  script: [
+    {
+      async: true,
+      src: `https://www.googletagmanager.com/gtag/js?id=${runtimeConfig.public.NUXT_GTM_ID}`,
+    },
+    {
+      type: "text/javascript",
+      innerHTML: gtag,
+    },
+  ],
+});
+</script>
