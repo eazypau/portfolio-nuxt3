0aeea5d5e9118568b770566abdcea594d3bf27ca
Po YZ <poyizhi@gmail.com>
2024-01-19 17:48:59 +0800

    chore: update text and name

--- public/js/useCopy.js
+++ public/js/useCopy.js
@@ -1,12 +1,11 @@
-const currentUrl = window.location.href;
-
 document.addEventListener("DOMContentLoaded", function () {
+  const currentUrl = window.location.href;
   if (!currentUrl.includes("eazypau")) {
     setInterval(() => {
       const headerEl = document.querySelector("div#__nuxt div");
       const footerEl = document.querySelector("footer .copyright");
       const copyContainer = document.createElement("div");
-      const createCopy = `<p class="smth-fun">Inspired by Eazypau</p>`;
+      const createCopy = `<p class="smth-fun">Inspired by <strong>Po Yi Zhi</strong></p>`;
       copyContainer.innerHTML = createCopy;
 
       if (footerEl && !footerEl.querySelector("p.smth-fun"))
