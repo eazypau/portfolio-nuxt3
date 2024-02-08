6c76379d85a6ebf7c512c7addbf133099f6334a7
Po YZ <poyizhi@gmail.com>
2024-02-08 18:44:25 +0800

    new: revamp home page design

--- components/Header.vue
+++ components/Header.vue
@@ -11,10 +11,11 @@
         alt="laptop with black background"
       />
     </div> -->
-    <TransitionGroup name="list" tag="div">
-      <div v-for="square in squares" :key="square" :style="square"></div>
+    <TransitionGroup name="list" tag="div" class="header-bg">
+      <div v-for="rectang in rectangs" :key="rectang" :style="rectang"></div>
+      <img v-if="showBgImg" src="/red-blue.jpg" width="1920" height="1280" alt="blue red fusion" />
     </TransitionGroup>
-    <div>
+    <div class="content">
       <h1 class="leading-3">
         <span class="text-4xl md:text-5xl font-semibold">{{
           introObj.lineOne
@@ -31,11 +32,19 @@
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
-const loading = ref(true);
-const squares = ref([]);
+// const loading = ref(true);
+
+const rectangs = ref([]);
 const counter = ref(0);
-const numOfSquares = ref(200);
+const numOfRectang = ref(4);
+const currentLeftPosition = ref(20)
+const width = 2
 const speed = 50;
+const rectangDelay = ref(1500)
+const typeWriterDelay = ref(2000)
+const showBgImg = ref(false)
+const { enabled } = useTheme()
+const { transitionCompletedOnce } = useTransitionTracking()
 
 const i = ref(0);
 const j = ref(0);
@@ -50,13 +59,13 @@ const intro = [
   "I specialise in building web app that helps to solve real world challenges.",
 ];
 
+
 const addSquare = () => {
-  if (counter.value < numOfSquares.value) {
-    const randomSize = Math.floor(Math.random() * (150 - 30) + 30);
-    const positionX = Math.floor(Math.random() * 100);
-    const positionY = Math.floor(Math.random() * 100);
-    const styling = `height: ${randomSize}px; width: ${randomSize}px; top: ${positionY}%; left: ${positionX}%`;
-    squares.value.push(styling);
+  if (counter.value < numOfRectang.value) {
+    const positionY = 0
+    const styling = `height: 100%; width: ${width}%; top: ${positionY}; left: ${currentLeftPosition.value}%`;
+    rectangs.value.push(styling);
+    currentLeftPosition.value += 20
     counter.value++;
     setTimeout(addSquare, speed);
   }
@@ -78,22 +87,68 @@ const typeWriter = () => {
   }
 };
 
-onMounted(() => {
+const assignDelayAmount = () => {
   if (window.innerWidth < 500) {
-    numOfSquares.value = 100;
+    if (enabled.value) {
+      typeWriterDelay.value = 300
+    } else {
+      rectangDelay.value = transitionCompletedOnce.value ? 0 : 600
+      typeWriterDelay.value = transitionCompletedOnce.value ? 400 : 1000
+    }
+  } else {
+    if (enabled.value) {
+      typeWriterDelay.value = transitionCompletedOnce.value ? 300 : 1200
+    } else {
+      rectangDelay.value = transitionCompletedOnce.value ? 0 : 1500
+      typeWriterDelay.value = transitionCompletedOnce.value ? 500 : 2000
+    }
   }
-  loading.value = false;
-  Promise.all([addSquare(), typeWriter()]);
+}
+
+watchEffect(() => {
+  if (counter.value === 4 && !enabled.value) {
+    setTimeout(() => {
+      showBgImg.value = true
+    }, 500);
+  } else if (enabled.value) {
+    setTimeout(() => {
+      showBgImg.value = true
+    }, 800);
+  }
+})
+
+onMounted(() => {
+  assignDelayAmount()
+  setTimeout(() => {
+    addSquare()
+  }, rectangDelay.value);
+  if (counter.value === 4 && !enabled.value) {
+    setTimeout(() => {
+      showBgImg.value = true
+    }, enabled.value ? 0 : 500);
+  } else if (enabled.value) {
+    setTimeout(() => {
+      showBgImg.value = true
+    }, 800);
+  }
+  setTimeout(() => {
+    typeWriter()
+  }, typeWriterDelay.value);
+  // loading.value = false;
+  // Promise.all([addSquare(), typeWriter()]);
 });
+
 </script>
 <style lang="scss" scoped>
 .headerHeight {
   height: calc(100vh - 70px);
 }
+
 .list-enter-active,
 .list-leave-active {
   transition: all 0.5s ease;
 }
+
 .list-enter-from,
 .list-leave-to {
   opacity: 0;
