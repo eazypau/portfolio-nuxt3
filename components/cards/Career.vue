9116d39d7ab99ec1f6c76ef31e36b7991983aabc
YiZhi_Rwk <yizhi.po@renalworks.com>
2026-06-19 17:32:55 +0800

    Feature: add animation in about

--- components/cards/Career.vue
+++ components/cards/Career.vue
@@ -10,14 +10,14 @@ const props = defineProps({
 </script>
 <template>
   <div
-    class="relative bg-white dark:bg-[#1c1c1c] border border-white hover:border-gray-600 dark:border-[#1c1c1c] dark:hover:border-white rounded shadow-lg mt-14 transition ease-out duration-300"
+    class="career-card relative bg-white dark:bg-[#1c1c1c] border border-white hover:border-gray-600 dark:border-[#1c1c1c] dark:hover:border-white rounded shadow-lg mt-14 transition ease-out duration-300"
   >
     <div class="w-full absolute -top-10">
       <img
         :src="careerDetails.logo"
         :alt="careerDetails.companyName"
         :style="`border-color: ${careerDetails.logoBorderColor};`"
-        class="w-20 rounded-full border mx-auto"
+        class="career-logo w-20 rounded-full border mx-auto"
       />
     </div>
     <div class="p-5">
